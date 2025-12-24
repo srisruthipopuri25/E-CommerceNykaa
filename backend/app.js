20d7306f9a31645de8f14f3509e817e74a6fe9bf
srisruthipopuri25 <srisruthipopuri@gmail.com>
2025-12-24 11:05:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,9 +20,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
