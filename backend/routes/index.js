c92db0d93dec134a242db34aaebea7f4d48b48d3
srisruthipopuri25 <srisruthipopuri@gmail.com>
2025-12-24 11:17:46 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home API */
+router.get('/', function(req, res) {
+  res.json({
+    success: true,
+    message: 'Backend API is running'
+  });
 });
 
 module.exports = router;
