8c212089ddf953c5e17f0ba8486afdf47ca8e293
srisruthipopuri25 <srisruthipopuri@gmail.com>
2025-12-24 11:13:36 +0530

    cleaned 500 error

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,69 @@
-
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-// 🔥 Add this line
-require("./db"); // Connect to MongoDB
+// Connect to MongoDB
+require("./db");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
+
 var app = express();
 
+/**
+ * CORS Configuration
+ * (Adjust origin for production)
+ */
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-
+/**
+ * Middlewares
+ */
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+/**
+ * Routes
+ */
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 
-// catch 404 and forward to error handler
+/**
+ * 404 Handler (API-style)
+ */
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
 });
 
-// error handler
+/**
+ * Global Error Handler (NO res.render)
+ */
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error"
+  });
 });
 
 module.exports = app;
