daca72a71d1f627b35cb13d024882a6daa30da79
sruthi <srisruthipopuri@gmail.com>
2026-01-02 17:53:00 +0530

    axios error

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI =
-  'mongodb+srv://srisruthipopuri:password@nykaa.8vv9hop.mongodb.net/nykaa?appName=nykaa';
+  'mongodb+srv://srisruthipopuri:Atchu77777@nykaa.8vv9hop.mongodb.net/nykaa?appName=nykaa';
 
 mongoose
   .connect(mongoURI)
